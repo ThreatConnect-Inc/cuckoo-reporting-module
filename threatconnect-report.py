439826b072ff0e2bf570bab3f23f171f10ee8407
Robert Simmons <rsimmons@threatconnect.com>
2016-06-30 17:55:25 -0400

    Add license

--- threatconnect-report.py
+++ threatconnect-report.py
@@ -1,4 +1,15 @@
-"""ThreatConnect reporting module for Cuckoo version 1.2.
+# Copyright 2016 ThreatConnect, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""
+ThreatConnect reporting module for Cuckoo version 1.2.
 
 This module creates an incident in ThreatConnect representing the analysis, and then imports all
 network indicators found by Cuckoo and associates those indicators with the analysis.
